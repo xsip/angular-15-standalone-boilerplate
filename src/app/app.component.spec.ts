f40f3b0f6a339cb6aa877a9db05ba7c22b372c6b
Kerschbaumer Stefan <xsip@pm.me>
2023-04-06 20:50:30 +0200

    sample layout

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,6 @@ describe('AppComponent', () => {
   it(`should have as title 'angular-15-boilerplate'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-15-boilerplate');
   });
 
   it('should render title', () => {
